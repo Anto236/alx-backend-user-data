1d20e8ee6609596fcc014a926fb70878f559bab0
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-08 13:09:49 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API"""
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
 
         base64_part = authorization_header[len("Basic "):]
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes a Base64 string and returns the decoded
+           value as UTF-8 string.
+        """
+        if base64_authorization_header is None or not \
+                isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_bytes.decode('utf-8')
+            return decoded_value
+        except base64.binascii.Error:
+            return None

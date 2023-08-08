696e3d54cf219d2f256da9def8d5174342caa6d5
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-08 10:41:33 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,21 @@ class Auth:
     """
     a class to manage the API authentication
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Placeholder method for authentication requirement """
-        return False
+        """ Check if authentication is required for the given path """
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path) or \
+                    path == excluded_path.rstrip('/'):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Placeholder method for retrieving authorization header """

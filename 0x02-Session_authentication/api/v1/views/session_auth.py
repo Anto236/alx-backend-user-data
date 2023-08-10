159d465894e9f9a315645e35b93b1858ba790a46
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-10 12:18:19 +0000

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,12 @@ def auth_session_login():
     response.set_cookie(getenv("SESSION_NAME"), session_id)
 
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def auth_session_logout():
+    """Handles session authentication for logout."""
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200

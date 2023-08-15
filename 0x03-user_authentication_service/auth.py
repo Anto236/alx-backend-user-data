2d2de5d341564b505c3524aac5cb0de39e06f215
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-15 19:15:33 +0000

    task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,7 @@ def _hash_password(password: str) -> bytes:
     """
     return hashpw(password.encode('utf-8'), gensalt())
 
+
 def _generate_uuid() -> str:
     """
     Generates a new UUID and returns its string representation.
